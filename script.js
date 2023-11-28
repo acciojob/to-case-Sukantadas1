19a2a5101c17ea7c88e73907f1c84ccccf86ce61
Sukanta Das <111263085+Sukantadas1@users.noreply.github.com>
2023-11-28 10:56:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-function toCase(text) {
-  // write your code here
+function toCase(inputStr) {
+    // Check for an empty string
+  if (inputStr === '') {
+    return '-';
+  }
+
+  // Convert the string to lowercase and uppercase with - as the delimiter
+  const result = `${inputStr.toLowerCase()}-${inputStr.toUpperCase()}`;
+
+  return result;
 }
 
 // DO not change the code below
